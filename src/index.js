ef73e65f941cf585550c121a914cf5999f0d972b
leylagahramanova <leylagahramanova13@gmail.com>
2023-12-27 00:56:11 +0400

    g

--- src/index.js
+++ src/index.js
@@ -10,14 +10,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Router>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </Router>,
   document.getElementById('root')
 );
 
