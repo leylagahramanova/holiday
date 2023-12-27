e23a2dfd52e6bfa7fbb62bcf5419d8b0e27659c0
leylagahramanova <leylagahramanova13@gmail.com>
2023-12-27 11:43:32 +0400

    hh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter,Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Teymur from './pages/Teymur';
 import Mahir from './pages/Mahir';
@@ -7,19 +7,19 @@ import Naza from './pages/Naza';
 import Mama from './pages/Mama';
 import Papa from './pages/Papa';
 import Card from './pages/Card';
-import { HashRouter  } from 'react-router-dom';
+
 function App() {
   return (
-    <HashRouter >
-      <Routes><Route  path='/' element={<Card />} />
-          <Route path='/mama' element={<Mama />} />
-          <Route path="/papa" element={<Papa />} />
-          <Route path="/naza" element={<Naza />} />
-          <Route path="/mahir" element={<Mahir />} />
-          <Route path="/teymur" element={<Teymur />} />
-          </Routes>
-          
-    </HashRouter>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Card />} />
+        <Route path='/mama' element={<Mama />} />
+        <Route path="/papa" element={<Papa />} />
+        <Route path="/naza" element={<Naza />} />
+        <Route path="/mahir" element={<Mahir />} />
+        <Route path="/teymur" element={<Teymur />} />
+      </Routes>
+    </div>
   );
 }
 
